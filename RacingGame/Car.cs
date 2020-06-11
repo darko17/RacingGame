c319320e0a1d05b4d8dabfe7976644709640d054
Darko Romovski <darko.finki@yahoo.com>
2020-06-11 21:27:05 +0200

    moving lanes init

---
+++ RacingGame/Car.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Drawing;
+
+namespace RacingGame
+{
+    public class Car
+    {
+        public Point Location { get; set; }
+        public int Delete { get; set; }
+
+        public static int drivingSpeed;
+
+        public Car(Point p)
+        {
+            Location = p;
+            Delete = 0;
+        }
+
+        public void Drive(int drivingSpeed)
+        {
+            Location = new Point(Location.X, Location.Y + drivingSpeed);
+        }
+    }
+}
---
+++ RacingGame/Line.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace RacingGame
+{
+    public class Line
+    {
+        Pen p = new Pen(Color.White);
+        Brush b = new SolidBrush(Color.White);
+        public Rectangle line;
+        public int y;
+
+        public Line(int y)
+        {
+            this.y = y;
+            line = new Rectangle(220, y, 4, 150);
+        }
+
+        public void Draw(Graphics g)
+        {
+            g.DrawRectangle(p, line);
+            g.FillRectangle(b, line);
+        }
+    }
+}
